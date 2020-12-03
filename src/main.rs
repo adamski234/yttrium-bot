53a37c9e4c565693e015d9546ad52e2f71d6aa13
adamski234 <adamski234@protonmail.com>
2020-12-03 12:17:37 +0100

    Works now

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,35 @@ use std::sync::Arc;
 use serde::{Deserialize, Serialize};
 use serenity::prelude::*;
 use serenity::async_trait;
+use yttrium_key_base::environment::{Environment, events};
 
 struct EventHandler;
 
 #[async_trait]
 impl serenity::client::EventHandler for EventHandler {
 	async fn message(&self, context: Context, message: serenity::model::channel::Message) {
-		if message.content == format!("{}ping", context.data.read().await.get::<Config>().unwrap().read().await.prefix) {
-			message.reply(&context.http, "Lmao no").await.unwrap();
-		};
-		//if message.content == config.pre
+		let prefix = context.data.read().await.get::<Config>().unwrap().read().await.prefix.clone();
+		if message.content.starts_with(&prefix) {
+			let split = message.content.split_whitespace().collect::<Vec<&str>>();
+			if split[0].ends_with("execute") {
+				let keys = yttrium::key_loader::load_keys("");
+				let db_manager = Box::from(yttrium_key_base::databases::JSONDatabaseManager::new_from_json("{}", "757679825661198347"));
+				let environment = Environment::new(events::EventType::Default, message.guild_id.unwrap(), &context, db_manager);
+				message.channel_id.say(&context.http, format!("{:#?}", yttrium::interpret_string(message.content, &keys.keys, environment))).await.unwrap();
+			}
+		}
 	}
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
 	let config_file = "./config.json5";
 	let input = std::fs::read_to_string(config_file).expect("Could not read the config file");
 	let config = json5::from_str::<Config>(&input).expect("The config file is invalid");
-	let mut client = futures::executor::block_on(serenity::Client::builder(&config.token).event_handler(EventHandler)).unwrap();
-	//client.type_map_insert::<Config>(config);
-	futures::executor::block_on(client.data.write()).insert::<Config>(Arc::new(RwLock::new(config)));
+	let framework = serenity::framework::StandardFramework::default();
+	let mut client = serenity::Client::builder(&config.token).event_handler(EventHandler).framework(framework).await.unwrap();
+	client.data.write().await.insert::<Config>(Arc::new(RwLock::new(config)));
+	client.start().await.unwrap();
 }
 
 #[derive(Debug, Hash, Clone, Serialize, Deserialize)]
