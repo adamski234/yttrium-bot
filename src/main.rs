3f8749e37f54e07c2d67df141c948a8b2e2b4685
adamski234 <adamski234@protonmail.com>
2020-12-05 17:13:26 +0100

    Rewrote to use framework

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,42 @@
 use std::sync::Arc;
 use serde::{Deserialize, Serialize};
-use serenity::prelude::*;
-use serenity::async_trait;
+use serenity::{
+	prelude::RwLock,
+	client::Context,
+	model::channel::Message,
+	framework::standard::{
+		Args,
+		CommandResult,
+		macros::{group, command},
+	}
+};
 use yttrium_key_base::environment::{Environment, events};
 
-struct EventHandler;
+#[group]
+#[commands(execute)]
+struct General;
 
-#[async_trait]
-impl serenity::client::EventHandler for EventHandler {
-	async fn message(&self, context: Context, message: serenity::model::channel::Message) {
-		let prefix = context.data.read().await.get::<Config>().unwrap().read().await.prefix.clone();
-		if message.content.starts_with(&prefix) {
-			let split = message.content.split_whitespace().collect::<Vec<&str>>();
-			if split[0].ends_with("execute") {
-				let keys = yttrium::key_loader::load_keys("");
-				//Placeholder manager
-				let db_manager = Box::from(yttrium_key_base::databases::JSONDatabaseManager::new_from_json("{}", "757679825661198347"));
-				let environment = Environment::new(events::EventType::Default, message.guild_id.unwrap(), &context, db_manager);
-				message.channel_id.say(&context.http, format!("{:#?}", yttrium::interpret_string(split[1..].join(" "), &keys.keys, environment))).await.unwrap();
-			}
-		}
-	}
+#[command]
+async fn execute(context: &Context, message: &Message, args: Args) -> CommandResult {
+	let keys = yttrium::key_loader::load_keys("");
+	//Placeholder manager
+	let db_manager = Box::from(yttrium_key_base::databases::JSONDatabaseManager::new_from_json("{}", "757679825661198347"));
+	let environment = Environment::new(events::EventType::Default, message.guild_id.unwrap(), &context, db_manager);
+	let output = yttrium::interpret_string(String::from(args.rest()), &keys.keys, environment);
+	message.channel_id.say(&context.http, format!("{:#?}", output)).await.unwrap();
+	return Ok(());
 }
 
 #[tokio::main]
 async fn main() {
 	let config_file = "./config.json5";
 	let input = std::fs::read_to_string(config_file).expect("Could not read the config file");
-	let config = json5::from_str::<Config>(&input).expect("The config file is invalid");
-	let framework = serenity::framework::StandardFramework::default();
-	let mut client = serenity::Client::builder(&config.token).event_handler(EventHandler).framework(framework).await.unwrap();
-	client.data.write().await.insert::<Config>(Arc::new(RwLock::new(config)));
+	let bot_config = json5::from_str::<Config>(&input).expect("The config file is invalid");
+	let framework = serenity::framework::StandardFramework::new().configure(|config| {
+		return config.prefix(&bot_config.prefix);
+	}).group(&GENERAL_GROUP);
+	let mut client = serenity::Client::builder(&bot_config.token).framework(framework).await.unwrap();
+	client.data.write().await.insert::<Config>(Arc::new(RwLock::new(bot_config)));
 	client.start().await.unwrap();
 }
 
