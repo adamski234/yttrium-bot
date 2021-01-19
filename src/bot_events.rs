64c079277a9fc0a4d6c79b22d7e7bdb3247fde25
adamski234 <adamski234@protonmail.com>
2021-01-19 14:51:46 +0100

    Removed pointless match structures

--- src/bot_events.rs
+++ src/bot_events.rs
@@ -30,32 +30,27 @@ impl EventHandler for Handler {
 	async fn channel_create(&self, context: serenity::client::Context, channel: &serenity::model::channel::GuildChannel) {
 		let lock = context.data.read().await;
 		let db = lock.get::<DB>().unwrap();
-		match get_event_code("ChannelCreate", &channel.guild_id.to_string(), db).await {
-			Some(code) => {
-				let db_manager = SQLDatabaseManager::new(channel.guild_id, db);
-				let event_info = events::EventType::ChannelCreate(events::ChannelCreateEventInfo::new(channel.id));
-				let environment = Environment::new(event_info, channel.guild_id, &context, db_manager);
-				let keys = lock.get::<KeyList>().unwrap();
-				let output = yttrium::interpret_string(code, keys, environment).await;
-				match output {
-					Ok(output) => {
-						match output.warnings {
-							Some(warns) => {
-								let message = format!("ChannelCreate event had the following warnings: ```{:#?}```\n{}", warns, output.result.message);
-								output.result.target.say(&context.http, message).await.unwrap();
-							}
-							None => {
-								output.result.target.say(&context.http, output.result.message).await.unwrap();
-							}
+		if let Some(code) = get_event_code("ChannelCreate", &channel.guild_id.to_string(), db).await {
+			let db_manager = SQLDatabaseManager::new(channel.guild_id, db);
+			let event_info = events::EventType::ChannelCreate(events::ChannelCreateEventInfo::new(channel.id));
+			let environment = Environment::new(event_info, channel.guild_id, &context, db_manager);
+			let keys = lock.get::<KeyList>().unwrap();
+			let output = yttrium::interpret_string(code, keys, environment).await;
+			match output {
+				Ok(output) => {
+					match output.warnings {
+						Some(warns) => {
+							let message = format!("ChannelCreate event had the following warnings: ```{:#?}```\n{}", warns, output.result.message);
+							output.result.target.say(&context.http, message).await.unwrap();
+						}
+						None => {
+							output.result.target.say(&context.http, output.result.message).await.unwrap();
 						}
 					}
-					Err(error) => {
-						unimplemented!("Error in channel_create: `{:#?}`", error);
-					}
 				}
-			}
-			None => {
-				return;
+				Err(error) => {
+					unimplemented!("Error in channel_create: `{:#?}`", error);
+				}
 			}
 		}
 	}
@@ -63,32 +58,27 @@ impl EventHandler for Handler {
 	async fn channel_delete(&self, context: serenity::client::Context, channel: &serenity::model::channel::GuildChannel) {
 		let lock = context.data.read().await;
 		let db = lock.get::<DB>().unwrap();
-		match get_event_code("ChannelDelete", &channel.guild_id.to_string(), db).await {
-			Some(code) => {
-				let db_manager = SQLDatabaseManager::new(channel.guild_id, db);
-				let event_info = events::EventType::ChannelDelete(events::ChannelDeleteEventInfo::new(channel.id));
-				let environment = Environment::new(event_info, channel.guild_id, &context, db_manager);
-				let keys = lock.get::<KeyList>().unwrap();
-				let output = yttrium::interpret_string(code, keys, environment).await;
-				match output {
-					Ok(output) => {
-						match output.warnings {
-							Some(warns) => {
-								let message = format!("ChannelCreate event had the following warnings: ```{:#?}```\n{}", warns, output.result.message);
-								output.result.target.say(&context.http, message).await.unwrap();
-							}
-							None => {
-								output.result.target.say(&context.http, output.result.message).await.unwrap();
-							}
+		if let Some(code) = get_event_code("ChannelDelete", &channel.guild_id.to_string(), db).await {
+			let db_manager = SQLDatabaseManager::new(channel.guild_id, db);
+			let event_info = events::EventType::ChannelDelete(events::ChannelDeleteEventInfo::new(channel.id));
+			let environment = Environment::new(event_info, channel.guild_id, &context, db_manager);
+			let keys = lock.get::<KeyList>().unwrap();
+			let output = yttrium::interpret_string(code, keys, environment).await;
+			match output {
+				Ok(output) => {
+					match output.warnings {
+						Some(warns) => {
+							let message = format!("ChannelCreate event had the following warnings: ```{:#?}```\n{}", warns, output.result.message);
+							output.result.target.say(&context.http, message).await.unwrap();
+						}
+						None => {
+							output.result.target.say(&context.http, output.result.message).await.unwrap();
 						}
 					}
-					Err(error) => {
-						unimplemented!("Error in channel_create: `{:#?}`", error);
-					}
 				}
-			}
-			None => {
-				return;
+				Err(error) => {
+					unimplemented!("Error in channel_create: `{:#?}`", error);
+				}
 			}
 		}
 	}
@@ -97,32 +87,27 @@ impl EventHandler for Handler {
 		let channel = channel.guild().unwrap();
 		let lock = context.data.read().await;
 		let db = lock.get::<DB>().unwrap();
-		match get_event_code("ChannelUpdate", &channel.guild_id.to_string(), db).await {
-			Some(code) => {
-				let db_manager = SQLDatabaseManager::new(channel.guild_id, db);
-				let event_info = events::EventType::ChannelUpdate(events::ChannelUpdateEventInfo::new(channel.id));
-				let environment = Environment::new(event_info, channel.guild_id, &context, db_manager);
-				let keys = lock.get::<KeyList>().unwrap();
-				let output = yttrium::interpret_string(code, keys, environment).await;
-				match output {
-					Ok(output) => {
-						match output.warnings {
-							Some(warns) => {
-								let message = format!("ChannelCreate event had the following warnings: ```{:#?}```\n{}", warns, output.result.message);
-								output.result.target.say(&context.http, message).await.unwrap();
-							}
-							None => {
-								output.result.target.say(&context.http, output.result.message).await.unwrap();
-							}
+		if let Some(code) = get_event_code("ChannelUpdate", &channel.guild_id.to_string(), db).await {
+			let db_manager = SQLDatabaseManager::new(channel.guild_id, db);
+			let event_info = events::EventType::ChannelUpdate(events::ChannelUpdateEventInfo::new(channel.id));
+			let environment = Environment::new(event_info, channel.guild_id, &context, db_manager);
+			let keys = lock.get::<KeyList>().unwrap();
+			let output = yttrium::interpret_string(code, keys, environment).await;
+			match output {
+				Ok(output) => {
+					match output.warnings {
+						Some(warns) => {
+							let message = format!("ChannelCreate event had the following warnings: ```{:#?}```\n{}", warns, output.result.message);
+							output.result.target.say(&context.http, message).await.unwrap();
+						}
+						None => {
+							output.result.target.say(&context.http, output.result.message).await.unwrap();
 						}
 					}
-					Err(error) => {
-						unimplemented!("Error in channel_create: `{:#?}`", error);
-					}
 				}
-			}
-			None => {
-				return;
+				Err(error) => {
+					unimplemented!("Error in channel_create: `{:#?}`", error);
+				}
 			}
 		}
 	}
